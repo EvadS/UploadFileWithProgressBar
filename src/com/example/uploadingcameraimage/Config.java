1a26c1030dfc434820e98192171ed6ba5da28ef7
Evads <Skiba.Evgeniy@gmail.com>
2017-01-16 09:45:18 +0200

    initial commit

---
+++ src/com/example/uploadingcameraimage/Config.java
@@ -0,0 +1,8 @@
+package com.example.uploadingcameraimage;
+
+public class Config {
+	// File upload url (replace the ip with your server address)
+	public static final String FILE_UPLOAD_URL = "http://192.168.0.101:28080/api/files/upload";
+	// Directory name to store captured images and videospublic
+	static final String IMAGE_DIRECTORY_NAME = "Android File Upload";
+}
